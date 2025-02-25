c4d373c73736bb69f270920d27b7eec5cf2a0e1a
Paula Garcia <pauligarcia_05@hotmail.com>
2025-02-24 22:05:56 -0300

    new functionalities added

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,17 +2,20 @@ import TodoItem from "./TodoItem";
 import { useTodos } from "./store/todo-context";
 
 const Todos = () => {
-  const { todoList, isLoading, handleInputChange } = useTodos();
+  const { todoList, isLoading, handleInputChange, handleCompleteTodo, deleteTodo } = useTodos();
 
   return (
-    <div className="w-full max-w-[48rem] p-4 flex flex-col items-center space-y-4 overflow-y-auto max-h-[60vh]">
+    <div className="w-full max-w-[48rem] p-4 flex flex-col items-center space-y-4 max-h-[60vh]">
       {isLoading && <h3>Loading todos...</h3>}
       {todoList.map((todo) => (
         <TodoItem
           key={todo._id}
           id={todo._id}
           title={todo.title}
+          isCompleted={todo.is_completed}
           handleChange={handleInputChange}
+          handleComplete={handleCompleteTodo}
+          handleDeleteTodo={deleteTodo}
         />
       ))}
     </div>
--- src/components/store/todo-context.jsx
+++ src/components/store/todo-context.jsx
@@ -6,6 +6,7 @@ export const TodoContext = createContext({
   isLoading: true,
   handleInputChange: () => {},
   addNewTodo: () => {},
+  deleteTodo: () => {},
 });
 
 // 2. Creamos el proveedor que es quien se encargara de realizar todas las tareas, y aceptamos children ya que este proveedor encapsulara los componentes que tienen que acceder al estado global
@@ -28,27 +29,44 @@ const TodosProvider = ({ children }) => {
     fetchTodos();
   }, []);
 
-  const editTodo = async (id, value) => {
+  const editTodo = async (id, updatedFields) => {
+    setTodoList((prev) =>
+      prev.map((item) =>
+        item._id === id ? { ...item, ...updatedFields } : item
+      )
+    );
+
+    const todo = todoList.find((item) => item._id === id);
+    if (!todo) return;
+
+    const updatedTodo = { ...todo, ...updatedFields };
     const response = await fetch(`https://api.nstack.in/v1/todos/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        title: value,
-        description: "",
-        is_completed: false,
-      }),
+      body: JSON.stringify(updatedTodo),
     });
     const data = await response.json();
-    console.log('DATA DESDE EDIT', data);
+    console.log("DATA DESDE EDIT", data);
+  };
+
+  const handleUpdateField = (id, updatedFields) => {
+    setTodoList((prev) =>
+      prev.map((item) =>
+        item._id === id ? { ...item, ...updatedFields } : item
+      )
+    );
+    editTodo(id, updatedFields);
   };
 
   const handleInputChange = (id, value) => {
-    setTodoList((prev) =>
-      prev.map((item) => (item._id === id ? { ...item, title: value } : item))
-    );
-    editTodo(id, value);
+    console.log("A CAMBIAR", id, value);
+    if (typeof value === "boolean") {
+      handleUpdateField(id, { is_completed: value });
+    } else {
+      handleUpdateField(id, { title: value });
+    }
   };
 
   const addNewTodo = async (value) => {
@@ -65,14 +83,31 @@ const TodosProvider = ({ children }) => {
     });
     const data = await response.json();
     fetchTodos();
-    console.log("DATA desde handleInputChange", data);
   };
   console.log("TodoList", todoList);
+
+  const deleteTodo = async (id) => {
+    console.log("DELETE ID", id);
+    const response = await fetch(`https://api.nstack.in/v1/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    fetchTodos();
+  };
 
   //3. Retornamos el objeto provider con su valor para solo importar en App TodoContext y no todo el objeto
   return (
     <TodoContext.Provider
-      value={{ todoList, isLoading, handleInputChange, addNewTodo }}
+      value={{
+        todoList,
+        isLoading,
+        handleInputChange,
+        addNewTodo,
+        deleteTodo,
+      }}
     >
       {children}
     </TodoContext.Provider>
