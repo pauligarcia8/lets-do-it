c222c7928d303924fc508c4a9ed432c39bfa90e8
Paula Garcia <pauligarcia_05@hotmail.com>
2025-02-28 12:14:01 -0300

    completed tasks list added

---
+++ src/components/CompletedTodos.jsx
@@ -0,0 +1,31 @@
+import { useTodos } from "./store/todo-context";
+
+const CompletedTodos = () => {
+  const { todoList, handleInputChange } = useTodos();
+
+  const completedTodos = todoList.filter((todo) => todo.is_completed === true);
+
+  return (
+    <div className="w-full max-w-[48rem] p-4 flex flex-col items-center space-y-4 max-h-[60vh]">
+      <h1 className="text-2xl font-bold mb-4">
+        {completedTodos.length > 0 ? "COMPLETED!" : `Nothing done yet...`}
+      </h1>
+      {completedTodos.map((todo) => (
+        <div
+          key={todo.title}
+          className="flex items-center justify-start w-full"
+        >
+          <input
+            type="checkbox"
+            className="w-4 h-4 cursor-pointer text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500"
+            onChange={(e) => handleInputChange(todo._id, e.target.checked)}
+            checked={todo.is_completed}
+          />
+          <h2 className="font-medium px-1">{todo.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CompletedTodos;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,13 +2,15 @@ import TodoItem from "./TodoItem";
 import { useTodos } from "./store/todo-context";
 
 const Todos = () => {
-  const { todoList, isLoading } = useTodos();
+  const { todoList, isLoading, handleInputChange } = useTodos();
+  const todosIncompleted = todoList.filter(
+    (todo) => todo.is_completed === false
+  );
 
-  console.log('Todo List', todoList)
   return (
     <div className="w-full max-w-[48rem] p-4 flex flex-col items-center space-y-4 max-h-[60vh]">
       {isLoading && <h3>Loading todos...</h3>}
-      {todoList.map((todo) => (
+      {todosIncompleted.map((todo) => (
         <TodoItem
           key={todo._id}
           id={todo._id}
